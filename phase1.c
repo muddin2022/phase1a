0a0b7db6fa5d686fac2832fd156c87c2479d00c8
Corey Ramirez <159506030+unrevealer@users.noreply.github.com>
2024-09-22 13:51:59 -0700

    spork progress

--- phase1.c
+++ phase1.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#include "phase1.h" 
+#include "phase1.h"
 
-struct PCB {
+struct PCB
+{
     int pid;
     int status;
     int priority;
     int stackSize;
     USLOSS_Context context;
-    void (*funcPtr) (void);
+    void (*funcPtr)(void);
     void stack[];
 }
 
 /* --- Global variables --- */
-PCB *currProc;                      
-PCB procTable[MAXPROC];             
-void initStack[USLOSS_MIN_STACK]    
-void *initStackPtr; 
-int nextPid = 1; 
+PCB *currProc;
+PCB procTable[MAXPROC];
+void initStack[USLOSS_MIN_STACK] void *initStackPtr;
+int nextPid = 1;
 
 /* --- Function prototypes --- */
 int getNextPid(void);
@@ -25,7 +25,8 @@ void restoreInterrupts(unsigned int);
 void init(void);
 
 /* --- Functions from spec --- */
-void phase1_init(void) {
+void phase1_init(void)
+{
     unsigned int oldPsr = disableInterrupts();
 
     // Initialize data structures
@@ -34,7 +35,7 @@ void phase1_init(void) {
     phase4_start_service_processes();
     phase5_start_service_processes();
     phase5_mmu_pageTable_alloc(int pid);
-    phase5_mmu_pageTable_free (int pid, USLOSS_PTE*);
+    phase5_mmu_pageTable_free(int pid, USLOSS_PTE *);
 
     memset(procTable, 0, sizeof(procTable));
     PCB initProc;
@@ -44,24 +45,47 @@ void phase1_init(void) {
 
     USLOSS_ContextInit();
 
-    restoreInterrupts(oldPsr); 
-    
-    USLOSS_ContextSwitch();   
+    restoreInterrupts(oldPsr);
+
+    USLOSS_ContextSwitch();
 }
 
-void init(void) {
-
-}
-
-/* --- Helper functions, not defined in spec --- */ 
 /*
- * Checks the nextPid to see if it maps to a position in the procTable 
+ * Create child proc of current proc
+ *
+ */
+int spork(char *name, int (*func)(void *), void *arg, int stacksize, int priority)
+{
+    int pid = getNextPid();
+    int slot = pid % MAXPROC;
+    if (procTable[slot].pid != 0 || strlen(name) > MAXNAME || priority < 1 || priority > 5)
+        return -1;
+    if (stacksize < USLOSS_MIN_STACK)
+        return -2;
+
+    PCB *newProc = &procTable[slot];
+
+    newProc->pid = pid;
+    newProc->priority = priority;
+
+    return pid;
+}
+
+void init(void)
+{
+}
+
+/* --- Helper functions, not defined in spec --- */
+/*
+ * Checks the nextPid to see if it maps to a position in the procTable
  * that is alredy filled. It keeps checking for a blank spot in the procTable and
  * updates the global variable.
  */
-int getNextPid(void) {
+int getNextPid(void)
+{
     // check if nextPid already in use
-    while (procTable[nextPid % MAXPROC].pid != 0) {
+    while (procTable[nextPid % MAXPROC].pid != 0)
+    {
         nextPid++;
     }
 }
@@ -70,7 +94,8 @@ int getNextPid(void) {
  * Disables interrupts and return the old PSR value, so that interrupts
  * can be restored in the future.
  */
-unsigned int disableInterrupts(void) {
+unsigned int disableInterrupts(void)
+{
     unsigned int oldPsr = USLOSS_PsrGet();
     USLOSS_PsrSet(oldPsr & ~USLOSS_PSR_CURRENT_INT);
     return oldPsr;
@@ -79,7 +104,7 @@ unsigned int disableInterrupts(void) {
 /*
  * Restores interrupts to the value they were before they got disabled.
  */
-void restoreInterrupts(unsigned int oldPsr) {
+void restoreInterrupts(unsigned int oldPsr)
+{
     USLOSS_PsrSet(oldPsr);
 }
-
