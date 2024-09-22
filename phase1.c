72f2e88f57346d9598dcf98706b433a7af93439c
Michelle <michelleuddin@arizona.edu>
2024-09-22 14:38:59 -0700

    implement phase1_init and init

--- phase1.c
+++ phase1.c
@@ -3,12 +3,12 @@
 
 struct PCB {
     int pid;
+    char *name[MAXNAME];
     int status;
     int priority;
     int stackSize;
     USLOSS_Context context;
     void (*funcPtr) (void);
-    void stack[];
 }
 
 /* --- Global variables --- */
@@ -32,31 +32,52 @@ void phase1_init(void) {
 
     PCB initProc;
     initProc.pid = getNextPid();
+    initProc.name = "init";
     initProc.priority = 6;
     initProc.stackSize = USLOSS_MIN_STACK;
     initProc.funcPtr = &init;
-    initProc.stack = initStack;
 
-    USLOSS_ContextInit();
+    int index = initProc.pid % MAXPROC;
+    procTable[index] = initProc;
 
-    restoreInterrupts(oldPsr); 
-    
-    USLOSS_ContextSwitch();   
+    USLOSS_ContextInit(&procTable[index].context, initStackPtr, USLOSS_MIN_STACK, NULL, initProc.funcPtr);
+
+    currProc = &initProc;
+    restoreInterrupts(oldPsr);   
 }
 
 void init(void) {
+    unsigned int oldPsr = disableInterrupts();
+
     // start services
     phase2_start_service_processes();
     phase3_start_service_processes();
     phase4_start_service_processes();
     phase5_start_service_processes();
-    phase5_mmu_pageTable_alloc(int pid);
-    phase5_mmu_pageTable_free (int pid, USLOSS_PTE*);
+    phase5_mmu_pageTable_alloc(currProc->pid);
+    phase5_mmu_pageTable_free (currProc->pid, NULL);
+
+    restoreInterrupts(oldPsr);
 
     // create testcase_main proc
+    spork("testcaseMain", &testcase_main, NULL, USLOSS_MIN_STACK, 3);
 
     // call join to clean up procTable
+    int joinRetVal = 1;
+    int status;
+    while (joinRetVal > 0) {
+        join(&status);
+    }
+
+    if (joinRetVal == -2) {
+        USLOSS_Console("ERROR: init has no more children, terminating simulation\n");
+        return; 
+
+    } else if (joinRetVal == -3) {
+        USLOSS_Console("ERROR: status pointer is null");
+    }
 }
+
 
 /* --- Helper functions, not defined in spec --- */ 
 /*
@@ -66,7 +87,7 @@ void init(void) {
  */
 int getNextPid(void) {
     // check if nextPid already in use
-    while (procTable[nextPid % MAXPROC].pid != 0) {
+    while (procTable[nextPid % MAXPROC]->pid != 0) {
         nextPid++;
     }
 }
